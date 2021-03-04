7d40a68b86a3da895dddd72f0e4f04357ee3afa6
Kaciras <Kaciras@protonmail.com>
2021-03-04 15:47:46 +0800

    update typescript types

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,16 +11,19 @@ declare namespace anchor {
     interface AnchorOptions {
         level?: number;
         slugify?(str: string): string;
+        uniqueSlugStartIndex?: number;
         permalink?: boolean;
         renderPermalink?(slug: string, opts: AnchorOptions, state: Core, idx: number): void;
         permalinkClass?: string;
+        permalinkSpace?: boolean;
         permalinkSymbol?: string;
         permalinkBefore?: boolean;
-        permalinkHref?(slug: string): string;
+        permalinkHref?(slug: string, state: Core): string;
+        permalinkAttrs?(slug: string, state: Core): Record<string, string>;
         callback?(token: Token, anchor_info: AnchorInfo): void;
     }
 }
 
-declare function anchor(md: MarkdownIt, opts: anchor.AnchorOptions): void;
+declare function anchor(md: MarkdownIt, opts?: anchor.AnchorOptions): void;
 
 export = anchor;
